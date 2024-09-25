1c9e008ae785a62840f10716538ee701878f1ac6
Rem M. <morrig407@gmail.com>
2024-09-25 14:57:12 +0700

    Add Redis

--- funbox/db.py
+++ funbox/db.py
@@ -8,16 +8,34 @@ def create_table_links():
                      (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         url VARCHAR(255) NOT NULL,
-                        added_at DATETIME DEFAULT NOW
+                        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
                      );
                      """)
     conn.commit()
     conn.close()
 
-def select_links():
+def select_links(start=None, end=None):
     conn = sqlite3.connect("mydb.sql")
     curs = conn.cursor()
-    curs.execute("SELECT DISTINCT url FROM links")
-    links = curs.fetchall()
+
+    sql = """
+        SELECT DISTINCT url
+        FROM links
+        WHERE created_at BETWEEN (?) and (?)
+    """
+
+    if start:
+        start_expr = "created_at >= (?)"
+    if end:
+        end_expr = "created_at < (?)"
+    curs.execute(sql, (start, end))
+    links = tuple(map(lambda x: x[0], curs.fetchall()))
     conn.close()
     return links or []
+
+def save_links(links):
+    conn = sqlite3.connect("mydb.sql")
+    curs = conn.cursor()
+    curs.executemany("INSERT INTO links (url) VALUES (?)", list(links))
+    conn.commit()
+    conn.close()
--- funbox/main.py
+++ funbox/main.py
@@ -1,9 +1,11 @@
-from funbox.db import create_table_links, select_links
 from flask import Flask, json, request
 from urllib.parse import urlparse
+from redis import Redis
+from datetime import datetime
 
 
 app = Flask(__name__)
+r = Redis('localhost', port=6379)
 
 
 def normalize_url(url):
@@ -15,7 +17,10 @@ def normalize_url(url):
 
 @app.get("/visited_links")
 def visited_links_get():
-    links = select_links()
+    start_date = request.args.get('from', 0)
+    end_date = request.args.get('to', -1)
+
+    links = [x.decode() for x in r.zrange('app:urls', start_date, end_date, withscores=False, byscore=True)]
     return {
             "domains": links,
             "status": "ok"
@@ -24,16 +29,14 @@ def visited_links_get():
 
 @app.post("/visited_links")
 def visited_links_post():
-    data = request.data
-    data = json.loads(data)
+    data = json.loads(request.data)
     links = data["links"]
-    normalized_links = set()
+    c = 0
     for link in links:
-        normalized_links.add(normalize_url(link))
+        c += r.zadd('app:urls', {normalize_url(link): int(datetime.timestamp(datetime.now()))})
+        print(datetime.timestamp(datetime.now()))
     return {}
 
 
 if __name__ == "__main__":
-    create_table_links()
-
     app.run()
